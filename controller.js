313bccc0f00a1522e101153036305a2325cdaf9b
Brandon Smith <Brandon Smith>
2017-04-26 12:37:24 -0600

    Solution

--- controller.js
+++ controller.js
@@ -1,10 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+angular.module('userProfiles').controller('MainController', function ($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+    $scope.getUsers = function () {
+        mainService.getUsers().then(function (response) {
+            console.log(response);
+            $scope.users = response.data.data;
+        })
+    }
 
 
-  $scope.getUsers();
+    $scope.getUsers();
 
 });
